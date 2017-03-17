b5d093ada3043ff9d8192cb37ef4af205bd706b4
shad-k <shadab.2005@gmail.com>
2017-03-17 13:18:55 +0530

    build: Add cssmin and uglify tasks

--- gruntFile.js
+++ gruntFile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
 
 
 	grunt.initConfig({
@@ -8,20 +9,38 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd: 'src/css',
-					src: ['*.css', '!*.min,css'],
+					src: ['*.css', '!*.min.css'],
 					dest: 'dist/css',
 					ext: '.min.css'
 				},
 				{
 					expand: true,
 					cwd: 'views/src/css',
-					src: ['*.css', '!*.min,css'],
+					src: ['*.css', '!*.min.css'],
 					dest: 'views/dist/css',
 					ext: '.min.css'
+				}]
+			}
+		},
+		uglify: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: 'src/js',
+					src: ['*.js','!*.min.js'],
+					dest: 'dist/js',
+					ext: '.min.js'
+				},
+				{
+					expand: true,
+					cwd: 'views/src/js',
+					src: ['*.js','!*.min.js'],
+					dest: 'views/dist/js',
+					ext: '.min.js'
 				}]
 			}
 		}
 	});
 
-	grunt.registerTask('default', ["cssmin"]);
+	grunt.registerTask('default', ["cssmin", "uglify"]);
 };
